5ad19171e2a48f09b27c03a67ae0226effe98d8a
Benjamin Nduagubu <nduagubu@gmail.com>
2023-09-30 16:50:18 -0400

    terraform-plan

---
+++ main.tf
@@ -0,0 +1,27 @@
+# Just a terraform file to practice Git Action
+provider "aws" {
+  region                   = "us-west-2"
+#  shared_credentials_files = ["/Users/19734/.aws/credentials"]
+}
+
+# terraform {
+#     backend "s3" {
+#         bucket = "jhooq-terraform-s3-bucket"
+#         key    = "jhooq/terraform/remote/s3/terraform.tfstate"
+#         region     = "us-west-2"
+#         #access_key = ""
+#         #secret_key = ""
+#     }
+# }
+
+
+resource "aws_instance" "ec2_example" {
+
+    ami = "ami-0767046d1677be5a0" 
+
+    instance_type = "t2.micro"
+
+    tags = {
+      Name = "EC2 Instance with remote state"
+    }
+}
