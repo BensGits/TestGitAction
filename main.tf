075ba4530ee0bc1ecf6b0a91d5b5365ea7093e8c
Benjamin Nduagubu <nduagubu@gmail.com>
2023-10-01 05:32:39 -0400

    commented-out3

--- main.tf
+++ main.tf
@@ -26,10 +26,10 @@ provider "aws" {
 #     }
 # }
 
-resource "aws_vpc" "test" {
-  cidr_block = "10.0.0.0/16"
+# resource "aws_vpc" "test" {
+#   cidr_block = "10.0.0.0/16"
 
-  tags = {
-    name = "Action-test"
-  }
-}
+#   tags = {
+#     name = "Action-test"
+#   }
+# }
